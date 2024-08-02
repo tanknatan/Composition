5a68557ed5a66d7f3e35560bab0d60a4e12a46f3
tanknatan <tanknatanrezet@gmail.com>
2024-08-02 13:46:12 +0300

    add xml and viewBinding

---
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -0,0 +1,30 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+import com.example.composition.databinding.FragmentGameBinding
+
+
+class GameFragment : Fragment() {
+    private var _binding: FragmentGameBinding? = null
+    private val binding : FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composition/presentation/MainActivity.kt
@@ -1,47 +1,13 @@
 package com.example.composition.presentation
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composition.ui.theme.CompositionTheme
+import com.example.composition.R
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CompositionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CompositionTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
